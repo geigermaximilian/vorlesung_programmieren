64842af145d70ee0851982831320dbea5d170a44
Maximilian Geiger <motto-lamellen-0f@icloud.com>
2026-02-03 10:45:55 +0100

    update Vorlesung 03.02.

---
+++ src/main/java/main/D02_StaticMethods.java
@@ -0,0 +1,15 @@
+package main;
+
+import main.helper.Printer;
+import utility.Calculator;
+
+public class D02_StaticMethods {
+
+    public static void main(String[] args){
+        Printer.printHelloWorld();
+        Printer.printText("test");;
+
+        int result = Calculator.add(5, 3);
+        System.out.println(result);
+    }
+}
