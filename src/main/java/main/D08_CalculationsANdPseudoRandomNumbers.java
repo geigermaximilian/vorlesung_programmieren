f324d0e23b798d7c64cb268cc59ed770144db602
Maximilian Geiger <motto-lamellen-0f@icloud.com>
2026-02-05 11:03:39 +0100

    Add D08_CalculationsANdPseudoRandomNumbers class with basic mathematical calculations

---
+++ src/main/java/main/D08_CalculationsANdPseudoRandomNumbers.java
@@ -0,0 +1,15 @@
+package main;
+
+/* Calculations and Pseudo Random Numbers
+ */
+
+public class D08_CalculationsANdPseudoRandomNumbers {
+    public static void main(String[] args) {
+        /* Mathematische Berechnungen */
+        double result = Math.sqrt(64);
+
+        System.out.println(result);
+
+        /*Pseudozufallszahlen */
+    }
+}
