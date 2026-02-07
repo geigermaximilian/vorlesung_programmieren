70274337aa8713461add1c833186b4521ae1bfc5
Maximilian Geiger <motto-lamellen-0f@icloud.com>
2026-02-07 20:10:52 +0100

    Add Operators01 class to demonstrate basic arithmetic operations in Java

---
+++ training/src/main/java/Operatoren/Operators01.java
@@ -0,0 +1,14 @@
+/**
+ * Aufgabe:
+ * https://jappuccini.github.io/java-docs/production/exercises/operators/operators01
+ */
+
+public class Operators01{
+    public void main(String[] args){
+        int a=13;
+        int b=4;
+
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
